555c3e71d0af62e56a9e7d40cb8e1dee06995e24
Monday <hencynikky@gmail.com>
2023-10-15 15:40:39 -0700

    test for custom strlen

--- print_f.c
+++ print_f.c
@@ -40,10 +40,12 @@ int _printf(const char *format, ...)
 				int str_len = 0;
 
 				while (str[str_len] != '\0')
+				{
 					str_len++;
 
 				write(1, str, str_len);
 				char_print += str_len;
+				}
 			}
 		}
 		format++;
